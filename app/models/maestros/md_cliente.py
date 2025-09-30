e73f5cdc45a26260305e2fa78113cf3aff866e6d
Luis Apaza <luis.apaza24@gmail.com>
2025-09-30 16:33:53 -0500

    campos

--- app/models/maestros/md_cliente.py
+++ app/models/maestros/md_cliente.py
@@ -4,10 +4,10 @@ class ClienteModel(db.Model):
     __tablename__ = "SA1010"
 
     cli_id = db.Column("R_E_C_N_O_", db.Integer, primary_key=True)
-    cod = db.Column("A1_COD",db.string(15))
-    nombre = db.Column("A1_NOME",db.string(80))
-    estado = db.Column("A1_MSBLQL",db.string(1))
-    delete=db.Column("D_E_L_E_T_",db.varchar(1))
+    cod = db.Column("A1_COD",db.String(15))
+    nombre = db.Column("A1_NOME",db.String(80))
+    estado = db.Column("A1_MSBLQL",db.String(1))
+    delete=db.Column("D_E_L_E_T_",db.String(1))
     
 
     
