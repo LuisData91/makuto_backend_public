480f801eb39e61dc7c732ae3d5e24c3d87720bfa
Luis Apaza <luis.apaza24@gmail.com>
2025-10-01 08:52:02 -0500

    LA TRANSACCIONES

--- app/models/maestros/__init__.py
+++ app/models/maestros/__init__.py
@@ -1,8 +1,7 @@
 from .md_cliente import ClienteModel
 from .md_vendedor import VendedorModel
 from .md_productos import ProductoModel
-from .md_cabRegIT import cabRegITModel
-from .md_det      import detModel
+
 from .md_tecnicos  import TecnicosModel
 from .md_familia  import FamiliaModel
 from .md_visita  import TipoVisitaModel
